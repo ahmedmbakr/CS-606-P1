860a526a22e8c70116566bf683b1341696aece87
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2023-09-25 13:45:35 -0500

    - Simple counting application for a server that accepts a specific number of clients requests (non-blocking)

--- client.py
+++ client.py
@@ -3,8 +3,14 @@ from constCS import * #-
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
-msg = "Hello World"     # compose a message
-s.send(msg.encode())    # send the message
-data = s.recv(1024)     # receive the response
-print(data.decode())    # print the result
+while True:
+    msg = input("Please enter a number: ")
+    print("Number received entered: ", msg)
+    if msg == "STOP":
+        break
+    s.send(msg.encode())    # send the message
+    print("Client sent {} to the server".format(msg))
+    data = s.recv(1024)     # receive the response
+    print("Client received result: {}".format(data))
+    print(data.decode())    # print the result
 s.close()               # close the connection
--- server.py
+++ server.py
@@ -1,13 +1,34 @@
+import threading
 from socket  import *
 from constCS import * #-
 
-s = socket(AF_INET, SOCK_STREAM) 
-s.bind((HOST, PORT))  #-
-s.listen(1)           #-
-(conn, addr) = s.accept()  # returns new socket and addr. client 
-while True:                # forever
-  data = conn.recv(1024)   # receive data from client
-  if not data: break       # stop if client stopped
-  msg = data.decode()+"*"  # process the incoming data into a response
-  conn.send(msg.encode())  # return the response
-conn.close()               # close the connection
+
+def client_thread_worker(msg):
+  return msg + 1
+
+
+def handle_client_request(conn, addr):
+  while True:
+    data = conn.recv(1024)  # receive data from client
+    if not data:
+      break  # stop if client stopped
+    msg = data.decode()  # process the incoming data into a response
+    current_thread = threading.current_thread()
+    thread_id = current_thread.getName()
+    print("Server counts {} in thread {} for connection {}:{}".format(msg, thread_id, addr[0], addr[1]))
+    updated_msg = client_thread_worker(msg)
+    conn.send(updated_msg.encode())  # return the response
+  print("Connection closed with {}:{}".format(addr[0], addr[1]))
+  conn.close()  # close the connection
+
+
+if __name__ == "__main__":
+  s = socket(AF_INET, SOCK_STREAM)
+  s.bind((HOST, PORT))  # AB: Bind the socket to specific IP address and port.
+  s.listen(MAX_NUM_CLIENT_CONNECTIONS_TO_SERVER)  # AB: maximum number of connection to be accepted by siumltaneously by the server.
+  while True:                # forever
+    print("Server listening on port: {} ...".format(PORT))
+    (conn, addr) = s.accept()  # returns new socket and addr. of the client
+    print("Accepted connection from {}:{}".format(addr[0], addr[1]))
+    client_handler = threading.Thread(target=handle_client_request, args=(conn, addr))
+    client_handler.start()
